f74e841342f0072a23e412d492d60a68df9e79d3
Anwar <anwarhussaini160@gmail.com>
2023-07-11 22:01:58 +0430

    Show a description on home menu item

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+function Home() {
+  return (
+    <div className="border border-success rounded p-2 box">
+      <h2 className="alert alert-success">Math Magician</h2>
+      <p className="fst-italic">
+        &#34;Introducing a sleek and user-friendly web app that combines the
+        power of a calculator with the inspiration of quotes. Harnessing
+        real-time data from an API, this app effortlessly performs calculations
+        while delivering motivational quotes, making number-crunching a
+        delightful and uplifting experience. Get ready to calculate and be
+        inspired in one place! &#34;
+      </p>
+    </div>
+  );
+}
+export default Home;
