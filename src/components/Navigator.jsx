f91cdd9d1907da008dba8fc146e3f1be76a9c580
Anwar <anwarhussaini160@gmail.com>
2023-07-13 21:03:15 +0430

    Change anchor tag to Link component for menu items

--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -1,23 +1,25 @@
+import { Link } from 'react-router-dom';
+
 function Navigator() {
   return (
     <div className="container py-4 bg-success mb-2 rounded">
       <nav className="d-flex justify-content-between align-items-center">
-        <a href="/" className="btn btn-light">
+        <Link to="/" className="btn btn-light">
           <h1>Math Magicians</h1>
-        </a>
+        </Link>
         <div className="list-group list-group-horizontal">
-          <a href="/" className="list-group-item list-group-item-action">
+          <Link to="/" className="list-group-item list-group-item-action">
             Home
-          </a>
-          <a
-            href="/calculator"
+          </Link>
+          <Link
+            to="/calculator"
             className="list-group-item list-group-item-action"
           >
             Calculator
-          </a>
-          <a href="/quotes" className="list-group-item list-group-item-action">
+          </Link>
+          <Link to="/quotes" className="list-group-item list-group-item-action">
             Quotes
-          </a>
+          </Link>
         </div>
       </nav>
     </div>
